bf8e1be1260c95757f9589816ae9c04b7e419ad0
Marlon Carrillo <marloncb2012@gmail.com>
2023-12-24 14:36:05 -0500

    initial file system

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AppShell,
+  Burger,
+  Group,
+  MantineProvider,
+  Skeleton,
+} from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import '@mantine/core/styles.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [opened, { toggle }] = useDisclosure()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider defaultColorScheme="dark">
+      <AppShell
+        header={{ height: 60 }}
+        footer={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened },
+        }}
+        aside={{
+          width: 300,
+          breakpoint: 'md',
+          collapsed: { desktop: false, mobile: true },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            Logo
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          Navbar
+          {Array(15)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+            ))}
+        </AppShell.Navbar>
+        <AppShell.Main>
+          Aside is hidden on on md breakpoint and cannot be opened when it is
+          collapsed
+        </AppShell.Main>
+        <AppShell.Aside p="md">Aside</AppShell.Aside>
+        <AppShell.Footer p="md">Footer</AppShell.Footer>
+      </AppShell.Header>
+    </MantineProvider>
   )
 }
 
